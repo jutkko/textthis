569dc62bc0b2414b7e8fedcc3605a8e82228549f
Zhou Yu <zhou@improbable.io>
2017-10-14 23:34:18 +0100

    Local version of the bot pretty much working, need to push it to the cloud

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	vision "cloud.google.com/go/vision/apiv1"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
@@ -32,10 +36,17 @@ func main() {
 		}
 
 		log.Printf("FILEPATH IS: %s", file.FilePath)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "YOLO")
-		msg.ReplyToMessageID = update.Message.MessageID
+		text := anylisePicture("https://api.telegram.org/file/bot" + os.Getenv("TGBOTAPITOKEN") + "/" + file.FilePath)
 
-		bot.Send(msg)
+		if len(text) > 0 {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text[0])
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			_, err := bot.Send(msg)
+			if err != nil {
+				log.Printf("Successfully sent response")
+			}
+		}
 	}
 }
 
@@ -50,7 +61,6 @@ func getPictureFile(bot *tgbotapi.BotAPI, update *tgbotapi.Update) (*tgbotapi.Fi
 		// The last element is the original picture
 		fileID = (*receivedMessage.Photo)[len((*receivedMessage.Photo))-1].FileID
 	} else if receivedMessage.Document != nil {
-		log.Printf("Got document file url: %v", receivedMessage.Document.FileID)
 		fileID = receivedMessage.Document.FileID
 	} else {
 		return nil, errors.New("No picture in the message")
@@ -62,4 +72,36 @@ func getPictureFile(bot *tgbotapi.BotAPI, update *tgbotapi.Update) (*tgbotapi.Fi
 	}
 
 	return &file, nil
+}
+
+func anylisePicture(address string) []string {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	resp, err := http.Get(address)
+	if err != nil {
+		log.Fatalf("Failed to get picture: %v", err)
+	}
+
+	image, err := vision.NewImageFromReader(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to create image: %v", err)
+	}
+
+	texts, err := client.DetectTexts(ctx, image, nil, 1)
+	if err != nil {
+		log.Fatalf("Failed to detect texts: %v", err)
+	}
+
+	result := []string{}
+	for _, text := range texts {
+		result = append(result, text.Description)
+	}
+
+	return result
 }
